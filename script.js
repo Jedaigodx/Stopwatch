4205d39534049ab959f6d32f21f8773409f0084d
Jedaigodx <thallisson.h.tp@gmail.com>
2025-04-21 00:51:30 -0300

    1.1

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ let min=0;
 let h=0;
 let intervalo
 
+function doisdigitos(digito){
+    if(digito<10){
+        return('0'+digito);
+    }
+    else{
+        return(digito);
+    }
+}
+
 function iniciar() {
-    intervalo = setInterval(watch,10)
+    intervalo = setInterval(watch,1000)
 }
 
 
@@ -15,21 +24,24 @@ function parar() {
 function zerar() {
     clearInterval(intervalo)
     seg=0;
+    document.getElementById("segundos").innerText="00";
     min=0;
+    document.getElementById("minutos").innerText="00";
     h=0;
+    document.getElementById("horas").innerText="00";
 }
 function watch(){
     seg++;
-    document.getElementById("segundos").innerText=seg;
+    document.getElementById("segundos").innerText=doisdigitos(seg);
     if(seg==60){
         seg=0;
         min++;
-        document.getElementById("minutos").innerText=min;
+        document.getElementById("minutos").innerText=doisdigitos(min);
         if(min==60)
         {
             min=0;
             h++;
-            document.getElementById("horas").innerText=h;
+            document.getElementById("horas").innerText=doisdigitos(h);
         }
     }
 }
